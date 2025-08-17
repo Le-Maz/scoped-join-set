989e600b38b93e24c1b1af271f564a2486fa2bc5
Le-Maz <mazurlech@proton.me>
2025-08-17 20:43:02 +0200

    Add a test covering a case where poll takes too long

---
+++ tests/long_poll.rs
@@ -0,0 +1,19 @@
+use std::{future::poll_fn, thread::sleep, time::Duration};
+
+use scoped_join_set::ScopedJoinSet;
+
+#[tokio::test]
+async fn long_poll() {
+    let local_variable = "local variable".to_string();
+    let mut scoped_join_set = ScopedJoinSet::new();
+    scoped_join_set.spawn(async {
+        poll_fn(|_| {
+            let reference = &local_variable;
+            sleep(Duration::from_millis(50));
+            std::task::Poll::Ready(reference.clone())
+        })
+        .await;
+    });
+    drop(scoped_join_set);
+    drop(local_variable);
+}
