8a291a8b51fef3df4ee3e2d0b00821ea6b6c73ff
Le-Maz <mazurlech@proton.me>
2025-11-29 13:38:09 +0100

    Add a second edge case test

---
+++ tests/edge_cases.rs
@@ -0,0 +1,65 @@
+use std::{
+    future::{poll_fn, Future},
+    task::Poll,
+    thread::sleep,
+    time::Duration,
+};
+
+use scoped_join_set::ScopedJoinSet;
+
+#[tokio::test(flavor = "multi_thread")]
+async fn long_poll() {
+    let local_variable = "local variable".to_string();
+    let mut scoped_join_set = ScopedJoinSet::new();
+    scoped_join_set.spawn(async {
+        poll_fn(|_| {
+            let reference = &local_variable;
+            sleep(Duration::from_millis(100));
+            Poll::Ready(reference.clone())
+        })
+        .await;
+    });
+    drop(scoped_join_set);
+    drop(local_variable);
+}
+
+#[tokio::test(flavor = "current_thread")]
+async fn special_drop() {
+    let local_variable = "local variable".to_string();
+    let mut scoped_join_set = ScopedJoinSet::new();
+
+    struct SpecialDrop<'a> {
+        reference: &'a str,
+    }
+
+    impl<'a> SpecialDrop<'a> {
+        fn new(reference: &'a str) -> Self {
+            Self { reference }
+        }
+    }
+
+    impl<'a> Future for SpecialDrop<'a> {
+        type Output = String;
+        fn poll(
+            self: std::pin::Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+        ) -> Poll<Self::Output> {
+            Poll::Pending
+        }
+    }
+
+    impl<'a> Drop for SpecialDrop<'a> {
+        fn drop(&mut self) {
+            self.reference.to_string();
+        }
+    }
+
+    scoped_join_set.spawn(async {
+        let f = SpecialDrop::new(&local_variable);
+        tokio::time::sleep(Duration::from_millis(20)).await;
+        f.await;
+    });
+
+    drop(scoped_join_set);
+    drop(local_variable);
+}
